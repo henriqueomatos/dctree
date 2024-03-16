966836d2398b992d7023daebcc4ac3715b51b0f5
henriqueomatos <aghenriquematos@gmail.com>
2024-03-16 16:58:41 -0300

    Criação e modificação de componentes dos links

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import './App.css';
+import Link from './components/Link';
+import LinkList from './components/LinkList';
 
-
+const links = [
+  {
+    label: "Quero aprender a programar"
+  },
+  {
+    label: "Quero aprender data analytics"
+  },
+  {
+    label: "Quero aprender marketing digital"
+  },
+  {
+    label: "Inscreva-se nos nossos cursos",
+    title: "Curso intensivo",
+    description:"Veja aqui quais os cursos intensivos estão com turma abertas e garanta a sua vaga."
+  },
+  {
+    label: "Clique e se inscreva",
+    title: "Digital Plus",
+    description: "Evento exclusivo para alunos!"
+  }
+  
+]
 
 function App() {
-    return (<div>Hello World</div>)
+    return(
+    <LinkList>
+      { links.map(item => <Link {...item} />)}
+    </LinkList>)
 }
 
 export default App
---
+++ src/components/LinkList/index.jsx
@@ -0,0 +1,10 @@
+import './LinkList.css';
+
+export default function LinkList({ children }){
+    return (
+        <div className="link-list">
+            { children }
+        </div>
+    )
+}
+
